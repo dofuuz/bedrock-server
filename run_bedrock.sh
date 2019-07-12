b96c3efc648436540534bfc3011858e2253687a2
dofuuz <dofu@nate.com>
2019-07-12 15:58:20 +0900

    Changed version checking method

--- run_bedrock.sh
+++ run_bedrock.sh
@@ -4,6 +4,9 @@
 # https://github.com/dofuuz/bedrock-server
 
 
+VersionTxt=server/version.txt
+CurrentVer=$(cat "$VersionTxt" 2> /dev/null)
+
 mkdir -p backup
 mkdir -p download
 
@@ -11,8 +14,7 @@ mkdir -p download
 # Create backup
 if [ -d "server" ]; then
     Timestamp=$(date +%Y%m%d-%H%M%S)
-    Ver=$(<server/version.txt)
-    BackupFile="backup/${Timestamp}_${Ver}.tar.gz"
+    BackupFile="backup/${Timestamp}_${CurrentVer}.tar.gz"
     echo "Creating backup to $BackupFile"
     tar -caf "$BackupFile" \
         server/worlds \
@@ -22,30 +24,29 @@ if [ -d "server" ]; then
 fi
 
 
-# Server update
+# Check latest version
 echo "Checking for the latest version..."
-
-# Download server index.html to check latest version
 curl -LsS -o download/version.html https://minecraft.net/en-us/download/server/bedrock/
 URL=$(grep -o 'https://minecraft.azureedge.net/bin-linux/[^"]*' download/version.html)
 if [ "$?" != 0 ]; then
     echo "WARNING: Unable to check version. Skipping update."
 else
-    DownloadZip=$(basename "$URL")
+    DownloadZip=download/$(basename "$URL")
     NewVer=$(basename "$URL" .zip)
 
-    # Download latest version of Bedrock dedicated server if a new one is available
-    if [ -f "download/$DownloadZip" ] && [ -d "server" ]; then
+    # Check for Latest server is installed
+    if [ "$CurrentVer" == "$NewVer" ]; then
         echo "Bedrock server is up to date."
     else
+        # Download and install/update server
         echo "New version $NewVer is available."
         echo "Downloading $URL"
-        curl -LsS -o "download/$DownloadZip" "$URL"
+        curl -L -o "$DownloadZip" "$URL"
 
         echo "Extracting..."
-        unzip -oq "download/$DownloadZip" -x server.properties permissions.json whitelist.json -d server
-        unzip -nq "download/$DownloadZip" server.properties permissions.json whitelist.json -d server
-        echo "$NewVer" > server/version.txt
+        unzip -oq "$DownloadZip" -x permissions.json server.properties whitelist.json -d server
+        unzip -nq "$DownloadZip" permissions.json server.properties whitelist.json -d server
+        echo "$NewVer" > "$VersionTxt"
     fi
 fi
 
@@ -53,3 +54,5 @@ echo
 
 cd server
 LD_LIBRARY_PATH=. ./bedrock_server
+
+echo
